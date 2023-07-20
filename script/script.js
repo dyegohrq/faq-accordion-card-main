def7bb9fa30d69cb7f44a44e62bc169979f79e27
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-07-20 16:34:23 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const question = document.getElementsByClassName('question-answeker')
+const content = document.getElementsByClassName('content')
+
+question.addEventListener("click", () => {
+    activate();
+})
+
+function activate() {
+    
+}
