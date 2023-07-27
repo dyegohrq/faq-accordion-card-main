f80c7089e4b5613f6ff7d5b48f397feb3e161c74
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-07-27 14:45:35 -0300

    .

--- script/script.js
+++ script/script.js
@@ -7,37 +7,3 @@ document.querySelectorAll(".question").forEach((question) => question.addEventLi
         question.parentNode.classList.add("active")
     }
 }))
-
-// // .question-answeker {
-// display: block;
-// width: 100%;
-// position: relative;
-// }
-// .accordion-questions img {
-// display: block;
-// position: absolute;
-// right: 0;
-// top: 7px;
-// cursor: pointer;
-// }
-
-// .question-answeker.active h3{
-// color: var(--Very-dark-desaturated-blue);
-// font-weight: 700;
-// }
-
-// .question-answeker.active .answer {
-// display: block;
-// }
-
-// .question-answeker.active img {
-// transform: rotate(180deg);
-// transition: all 0.4s ease-in;
-// }
-
-// .answer {
-// margin-bottom: 20px;
-// color: var(--Very-dark-grayish-blue);
-// width: 259px;
-// display: none;
-// }
